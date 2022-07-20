e6527ec08537be33781ee9938e825506f293d5a6
gjheij <j.heij@spinozacentre.nl>
2022-07-20 16:04:55 +0200

    uncomment tr /= 1000 as it interferes with high-pass filter

--- pybest/preproc.py
+++ pybest/preproc.py
@@ -79,15 +79,15 @@ def _run_func_parallel(ddict, cfg, run, func, logger):
     if 'fs' in cfg['space']:  # assume gifti
         if cfg['iscifti'] == 'y':
             data, tr = load_and_split_cifti(func, cfg['atlas_file'],cfg, cfg['left_id'], cfg['right_id'], cfg['subc_id'], cfg['mode'])
-            #tr /= 1000 # defined in msec
+            tr /= 1000 # defined in msec
         else:
             data, tr = load_gifti(func, cfg, return_tr=True)
-            #tr /= 1000  # defined in msec
+            tr /= 1000  # defined in msec
     else:
         # Load/mask data and extract stuff
         tr = nib.load(func).header['pixdim'][4]
         data = masking.apply_mask(func, ddict['mask'])
-
+    
     # By now, data is a 2D array (time x voxels)
     data = hp_filter(data, tr, ddict, cfg)
     
